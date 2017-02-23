d10cbec2907d0d2934e07dba6f78f38a62589181
Justin Chang <JustinChangMusic@gmail.com>
2017-02-23 17:46:23 -0500

    Made tests for Game. Time to make for all other objectS

--- server/game/test/game.js
+++ server/game/test/game.js
@@ -4,31 +4,57 @@
 
     describe('Game', function() {
         describe('#isAlive()', function() {
-            it('should return true when budget > -100,000', () => {
-                var g = new Game();
-                g.budget = 75000;
-                assert.equal(true, g.isAlive());
-                g.budget = 0;
-                assert.equal(true, g.isAlive());
-                g.budget = -1;
-                assert.equal(true, g.isAlive());
-                g.budget = -99999;
-                assert.equal(true, g.isAlive());
+            var g = new Game();
+            g.employees = [1];
+            g.vehicles = [];
 
-                g.budget = -100000;
+            it('should return true when budget = 75000', () => {
+                g.company.budget = 75000;
+                assert.equal(true, g.isAlive());
+            });
+
+            it('should return true when budget = 0', () => {
+                g.company.budget = 0;
+                assert.equal(true, g.isAlive());
+            });
+
+            it('should return true when budget = -1', () => {
+                g.company.budget = -1;
+                assert.equal(true, g.isAlive());
+            });
+
+            // Remember, isAlive() subtracts budgetAfterRound
+            // and then checks if > -100,000
+            it('should return false when budget = -99999', () => {
+                g.company.budget = -99999;
                 assert.equal(false, g.isAlive());
-                g.budget = -200000;
+            });
+
+            it('should return false when budget = -100000', () => {
+                g.company.budget = -100000;
+                assert.equal(false, g.isAlive());
+            });
+
+            it('should return false when budget = -200000', () => {
+                g.company.budget = -200000;
                 assert.equal(false, g.isAlive());
             });
         });
 
         describe('#nextRound()', function() {
-            it('should increment round if budget > -100,000', () => {
-                var g = new Game();
-                g.budget = 99999999; // make sure we have enough money
+            var g = new Game();
+            g.company.budget = 99999999; // make sure we have enough money
+
+            it('should start at round 1', () => {
                 assert.equal(1, g.round);
+            });
+
+            it('should increment round. round 2', () => {
                 g.nextRound();
-                assert.equal(2, g.round;
+                assert.equal(2, g.round);
+            });
+
+            it('should increment round. round 3', () => {
                 g.nextRound();
                 assert.equal(3, g.round);
             });
